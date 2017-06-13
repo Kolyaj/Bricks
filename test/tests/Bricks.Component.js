e491482f9be462c20e0796f14d7fa9643c3abb49
Kolyaj <kolyaj@yandex.ru>
2017-06-13 08:36:53 +0200

    Перенес методы _on и _un из Bricks.Widget в Bricks.Component

---
+++ test/tests/Bricks.Component.js
@@ -0,0 +1,51 @@
+(function() {
+    //#require Bricks.Component
+    describe('Bricks.Component', function() {
+        it('Passing config', function() {
+            var c = new Bricks.Component({
+                foo: 5
+            });
+            chai.assert.equal(c.foo, 5);
+        });
+
+        it('Prototype properties', function() {
+            var C = Bricks.Component.inherit({
+                a: 5,
+                b: 6
+            });
+            var c = new C({a: 7});
+            chai.assert.equal(c.a, 7);
+            chai.assert.equal(c.b, 6);
+        });
+
+        it('Method listeners', function() {
+            var c1 = new Bricks.Component();
+            var foo;
+            var C2 = Bricks.Component.inherit({
+                a: 5,
+
+                b: 6,
+
+                _initComponent: function() {
+                    C2.superclass._initComponent.apply(this, arguments);
+                    this._on(c1, 'event1', this.method1);
+                    this._on(c1, 'event2', this.method2);
+                },
+
+                method1: function() {
+                    foo = this.a;
+                },
+
+                method2: function() {
+                    foo = this.b;
+                }
+            });
+            var c2 = new C2();
+            c1._fireEvent('event1');
+            chai.assert.equal(foo, 5);
+            c2.destroy();
+            c1._fireEvent('event2');
+            chai.assert.equal(foo, 5);
+        });
+    });
+})();
---
+++ test/tests/Bricks.Observer.js
@@ -0,0 +1,51 @@
+(function() {
+    //#require Bricks.Observer
+    describe('Bricks.Observer', function() {
+        it('Single event', function() {
+            var o = new Bricks.Observer();
+            var hasEvent = false;
+            o.addEventListener('event', function() {
+                hasEvent = true;
+            });
+            o._fireEvent('event');
+            chai.assert.isOk(hasEvent);
+        });
+
+        it('Passing event object', function() {
+            var o = new Bricks.Observer();
+            var target;
+            var type;
+            o.addEventListener('event', function(evt) {
+                target = evt.target;
+                type = evt.type;
+            });
+            o._fireEvent('event');
+            chai.assert.equal(target, o);
+            chai.assert.equal(type, 'event');
+        });
+
+        it('Passing data', function() {
+            var o = new Bricks.Observer();
+            var foo;
+            o.addEventListener('event', function(evt) {
+                foo = evt.foo;
+            });
+            o._fireEvent('event', {foo: 5});
+            chai.assert.equal(foo, 5);
+        });
+
+        it('Removing listener', function() {
+            var o = new Bricks.Observer();
+            var foo = 0;
+            var listener = function() {
+                foo++;
+            };
+            o.addEventListener('event', listener);
+            o._fireEvent('event');
+            chai.assert.equal(foo, 1);
+            o.removeEventListener('event', listener);
+            o._fireEvent('event');
+            chai.assert.equal(foo, 1);
+        });
+    });
+})();

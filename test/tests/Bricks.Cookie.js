be753397ce88e942bdd6ac14328b8b811904ad70
Kolyaj <kolyaj@yandex.ru>
2017-08-07 09:44:05 +0300

    Научился куки ставить на поддомен.

---
+++ test/tests/Bricks.Cookie.js
@@ -0,0 +1,31 @@
+(function() {
+    //#require Bricks.Cookie.createCookie
+    //#require Bricks.Cookie.readCookie
+    //#require Bricks.Cookie.eraseCookie
+
+    describe('Bricks.Cookie', function() {
+        it('Create session cookie', function() {
+            Bricks.Cookie.createCookie('foo', 'bar');
+            chai.assert.equal(Bricks.Cookie.readCookie('foo'), 'bar');
+        });
+
+        it('Erase cookie', function() {
+            Bricks.Cookie.createCookie('foo', 'bar');
+            Bricks.Cookie.eraseCookie('foo');
+            chai.assert.equal(Bricks.Cookie.readCookie('foo'), null);
+        });
+
+        it('Create ttl cookie', function(callback) {
+            Bricks.Cookie.createCookie('foo', 'bar', 1 / 86400); // Ставим куку на одну секунду
+            chai.assert.equal(Bricks.Cookie.readCookie('foo'), 'bar');
+            setTimeout(function() {
+                chai.assert.equal(Bricks.Cookie.readCookie('foo'), null);
+                callback();
+            }, 1000)
+        });
+
+        // Просто создаём две куки, на текущий домен и на все поддомены
+        Bricks.Cookie.createCookie('foo1', 'bar1');
+        Bricks.Cookie.createCookie('foo2', 'bar2', null, '.bricks.tests');
+    });
+})();

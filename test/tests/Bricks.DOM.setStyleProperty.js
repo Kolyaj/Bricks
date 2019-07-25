6569cced86648ce29c3adcaa4507e58f0c319515
Kolyaj <kolyaj@yandex.ru>
2019-07-25 16:41:03 +0300

    * Новый метод Bricks.DOM.setStyleProperty с расширенной функциональностью

---
+++ test/tests/Bricks.DOM.setStyleProperty.js
@@ -0,0 +1,27 @@
+(function() {
+    //#imports
+    describe('Bricks.DOM.setStyleProperty', function() {
+        it('height: 100px', function() {
+            var el = document.createElement('div');
+            Bricks.DOM.setStyleProperty(el, 'height', '100px');
+            document.body.appendChild(el);
+            assert.equal(el.offsetHeight, 100);
+            document.body.removeChild(el);
+        });
+        it('height: 100', function() {
+            var el = document.createElement('div');
+            Bricks.DOM.setStyleProperty(el, 'height', 100);
+            document.body.appendChild(el);
+            assert.equal(el.offsetHeight, 100);
+            document.body.removeChild(el);
+        });
+        it('width,height: [100, 100]', function() {
+            var el = document.createElement('div');
+            Bricks.DOM.setStyleProperty(el, 'width,height', [100, 100]);
+            document.body.appendChild(el);
+            assert.equal(el.offsetWidth, 100);
+            assert.equal(el.offsetHeight, 100);
+            document.body.removeChild(el);
+        });
+    });
+})();

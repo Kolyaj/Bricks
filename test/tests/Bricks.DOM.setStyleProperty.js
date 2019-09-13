ba35c7d1f2507988ea255c46f98eb9f5f69bfd2e
Kolyaj <kolyaj@yandex.ru>
2019-09-13 10:42:50 +0300

    bugfix: нельзя было поставить значением css-свойства число 0

--- test/tests/Bricks.DOM.setStyleProperty.js
+++ test/tests/Bricks.DOM.setStyleProperty.js
@@ -23,5 +23,13 @@
             assert.equal(el.offsetHeight, 100);
             document.body.removeChild(el);
         });
+        it('width,height: [0, 0]', function() {
+            var el = document.createElement('div');
+            Bricks.DOM.setStyleProperty(el, 'width,height', [0, 0]);
+            document.body.appendChild(el);
+            assert.equal(el.offsetWidth, 0);
+            assert.equal(el.offsetHeight, 0);
+            document.body.removeChild(el);
+        });
     });
 })();
